9c527c4bc5fcd8b6c01e1c72138953bed0f03870
Ismael Ortega <chaoscatorce14@gmail.com>
2022-02-06 12:28:31 +0100

    Inital commit

---
+++ src/test/java/com/uned/tfm/tfm2022/Tfm2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uned.tfm.tfm2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tfm2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
